ec56ae5b43d54ac3a459f8b995496d82e162dad7
piotrulos <piotr.wwww@gmail.com>
2026-01-28 18:17:07 +0100

    version bump

--- MSCCoreLibrary/Properties/AssemblyInfo.cs
+++ MSCCoreLibrary/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.12")]
+[assembly: AssemblyFileVersion("1.0.2.15")]
